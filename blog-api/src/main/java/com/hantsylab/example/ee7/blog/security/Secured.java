f9ac6d37b29844e9c97a7d7e8262bdedda2b1112
hantsy <hantsy@gmail.com>
2016-07-01 22:54:36 +0800

    add basic authentication and authorization for jaxrs

---
+++ blog-api/src/main/java/com/hantsylab/example/ee7/blog/security/Secured.java
@@ -0,0 +1,21 @@
+package com.hantsylab.example.ee7.blog.security;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import java.lang.annotation.Retention;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import java.lang.annotation.Target;
+
+import javax.ws.rs.NameBinding;
+
+/**
+ *
+ * @author hantsy
+ */
+@NameBinding
+@Retention(RUNTIME)
+@Target({TYPE, METHOD})
+public @interface Secured {
+
+    Role[] value() default {};
+}
