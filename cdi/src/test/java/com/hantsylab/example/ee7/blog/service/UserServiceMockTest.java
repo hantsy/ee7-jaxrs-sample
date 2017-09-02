0b0d7d6774934c294119234d366b9559a1b4ecfd
hantsy <hantsy@gmail.com>
2017-09-02 14:14:08 +0800

    updated Mockito to the latest version, and removed deprecated APIs

--- cdi/src/test/java/com/hantsylab/example/ee7/blog/service/UserServiceMockTest.java
+++ cdi/src/test/java/com/hantsylab/example/ee7/blog/service/UserServiceMockTest.java
@@ -15,8 +15,7 @@ import org.mockito.Mock;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.when;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Matchers.anyLong;
-import static org.mockito.Matchers.anyString;
+
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.MockitoAnnotations.initMocks;
@@ -24,7 +23,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 
 /**
